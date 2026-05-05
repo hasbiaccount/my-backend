cf6882169ca365a24de3290b3a2ffbbb0afdeadf
Weedanta <baguswedanta@student.ub.ac.id>
2026-05-05 19:01:36 +0700

    refactor: rename CartAcara controller and seeder to Cart for consistency

--- database/migrations/2026_05_04_141834_create_carts_table.php
+++ database/migrations/2026_05_04_141834_create_carts_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('cart_acaras', function (Blueprint $table) {
+        Schema::create('carts', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->foreignId('event_id')->constrained()->cascadeOnDelete();
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('cart_acaras');
+        Schema::dropIfExists('carts');
     }
 };
